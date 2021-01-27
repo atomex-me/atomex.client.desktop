8071c43b6b96b4e9bb06ae8253837a6503b66d4c
Konstantin <johnysalvesen@gmail.com>
2021-01-27 16:04:04 +0300

    Initial commit;

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Atomex.Core;
+
+namespace Atomex.Client.Desktop.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => $"Welcome to Avalonia! {net.ToString()}";
+        private Network net = Network.MainNet;
+    }
+}
