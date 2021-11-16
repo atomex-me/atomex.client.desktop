9dfff06266daee3966837c3bb39078d653cfb24a
Konstantin <johnysalvesen@gmail.com>
2021-11-16 21:30:41 +0300

    Fixed bug with inactivity lockscreen and modals;

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -312,7 +312,7 @@ namespace Atomex.Client.Desktop.ViewModels
                     currenciesProvider: AtomexApp.CurrenciesProvider,
                     clientType: ClientType.Unknown);
             }, async () => await SignOut());
-            
+
             var wasClosed = false;
             Dispatcher.UIThread.InvokeAsync(() =>
             {
