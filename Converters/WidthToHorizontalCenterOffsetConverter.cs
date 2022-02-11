a08089d67dcd1cce34c30314ba2b4b4798e20631
Igor Matsak <matsak.i.v@gmail.com>
2022-02-11 17:21:40 +0300

    Fixes ToolTips, fixes converters

---
+++ Converters/WidthToHorizontalCenterOffsetConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+using Avalonia.Data.Converters;
+
+namespace Atomex.Client.Desktop.Converters
+{
+    public class WidthToHorizontalCenterOffsetConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var doubleValue = (double)value;
+
+            if (parameter != null && double.TryParse(parameter.ToString(), out var offset))
+                return -doubleValue / 2 + offset;
+
+            return -doubleValue / 2;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
