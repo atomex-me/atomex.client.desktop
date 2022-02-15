7666cb8d43b3232a92e7def1cfc505c2797ace86
Konstantin <johnysalvesen@gmail.com>
2022-02-15 17:43:28 +0300

    Fix/skiasharp render bugs (#29)
    * Updated Avalonia version to 0.10.12;
    
    * Removed code that temporarily fixed skiasharp render bugs;
    
    * Added StartPage class for RoundedButton;

--- Services/DialogService.cs
+++ Services/DialogService.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Threading.Tasks;
 using Atomex.Client.Desktop.Dialogs.ViewModels;
-using Avalonia.Controls;
 using Atomex.Client.Desktop.ViewModels;
-using Avalonia.Threading;
-using Serilog;
 
 
 namespace Atomex.Client.Desktop.Services
@@ -12,25 +7,12 @@ namespace Atomex.Client.Desktop.Services
     public sealed class DialogService
     {
         private static string MainDialogHostIdentifier => "MainDialogHost";
-        private readonly Window _owner;
         private bool _isDialogOpened;
-        private readonly bool _isLinux;
         private readonly DialogServiceViewModel _dialogServiceViewModel;
 
-        public DialogService(Window owner, bool isLinux)
+        public DialogService()
         {
-            _owner = owner;
-            _isLinux = isLinux;
             _dialogServiceViewModel = new DialogServiceViewModel();
-        }
-
-        private async void ReRender()
-        {
-            if (!_isLinux) return;
-            Random random = new Random();
-            var opacity = random.NextDouble() * (0.9999 - 0.9990) + 0.9990;
-            await Task.Delay(50);
-            await Dispatcher.UIThread.InvokeAsync(() => _owner.Opacity = opacity);
         }
 
         public bool Close()
@@ -53,8 +35,6 @@ namespace Atomex.Client.Desktop.Services
             if (_isDialogOpened) return;
             _ = DialogHost.DialogHost.Show(_dialogServiceViewModel, MainDialogHostIdentifier);
             _isDialogOpened = true;
-
-            ReRender();
         }
 
 
