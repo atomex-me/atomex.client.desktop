5538e7c5985b89042118e4df4c4788a57f527fe3
Konstantin <johnysalvesen@gmail.com>
2022-03-10 17:04:27 +0300

    ManageAssets layout done;

--- ViewModels/ManageAssetsViewModel.cs
+++ ViewModels/ManageAssetsViewModel.cs
@@ -1,12 +1,11 @@
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using Atomex.Client.Desktop.ViewModels.CurrencyViewModels;
-using Atomex.Core;
 using ReactiveUI.Fody.Helpers;
+
 
 namespace Atomex.Client.Desktop.ViewModels
 {
-    public class CurrencyWithSelection
+    public class CurrencyWithSelection : ViewModelBase
     {
         public CurrencyViewModel Currency { get; set; }
         [Reactive] public bool IsSelected { get; set; }
@@ -14,13 +13,7 @@ namespace Atomex.Client.Desktop.ViewModels
 
     public class ManageAssetsViewModel : ViewModelBase
     {
-        public ObservableCollection<CurrencyWithSelection> AvailableCurrencies { get; set; }
-        public ObservableCollection<CurrencyWithSelection> SelectedCurrencies { get; set; }
+        [Reactive] public ObservableCollection<CurrencyWithSelection> AvailableCurrencies { get; set; }
         [Reactive] public string SearchPattern { get; set; }
-
-        public ManageAssetsViewModel()
-        {
-            SelectedCurrencies = new ObservableCollection<CurrencyWithSelection>();
-        }
     }
 }
