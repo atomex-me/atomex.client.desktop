80ba1f40f1eee60022c0e94a1b3922e1a6b7ecb8
Konstantin <johnysalvesen@gmail.com>
2022-06-03 16:37:50 +0300

    BTC select outputs now disable confirm button if 0 outputs selected; (#49)

---
+++ Converters/NumericConverters.cs
@@ -0,0 +1,30 @@
+using Avalonia.Data.Converters;
+
+
+namespace Atomex.Client.Desktop.Converters
+{
+    public static class NumericConverters
+    {
+        public static readonly IValueConverter GreaterThanZero =
+            new FuncValueConverter<object, bool>(val =>
+            {
+                return val switch
+                {
+                    decimal decimalValue => decimalValue > 0,
+                    int intValue => intValue > 0,
+                    _ => false
+                };
+            });
+
+        public static readonly IValueConverter LowerThanZero =
+            new FuncValueConverter<object, bool>(val =>
+            {
+                return val switch
+                {
+                    decimal decimalValue => decimalValue < 0,
+                    int intValue => intValue < 0,
+                    _ => false
+                };
+            });
+    }
+}
