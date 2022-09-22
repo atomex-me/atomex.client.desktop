6d91b646dc209029dff220b968eedb9e3995946f
Konstantin <johnysalvesen@gmail.com>
2022-09-22 15:41:07 +0300

    Fix bug with copy operation request addr;

--- ViewModels/DappsViewModels/DappsViewModel.cs
+++ ViewModels/DappsViewModels/DappsViewModel.cs
@@ -397,8 +397,8 @@ namespace Atomex.Client.Desktop.ViewModels.DappsViewModels
                             StorageLimit = 5000,
                             Parameters = new Parameters
                             {
-                                Entrypoint = o.Parameters["entrypoint"]!.ToString(),
-                                Value = Micheline.FromJson(o.Parameters["value"]!.ToString())
+                                Entrypoint = o?.Parameters?["entrypoint"]!.ToString(),
+                                Value = Micheline.FromJson(o?.Parameters?["value"]!.ToString())
                             }
                         };
                     }));
